c89d7d8844cfd25770c747b660a723b7d676e95c
Dominik <dominik.zulovecsajovic@mynestboxco.uk>
2024-08-15 10:12:55 +0100

    pushed the lambda with proper dependencies

--- lambdas/sql-execute/deploy.sh
+++ lambdas/sql-execute/deploy.sh
@@ -3,8 +3,14 @@
 # Navigate to the lambda function directory
 cd lambdas/sql-execute
 
+# Clean up any previous package directory
+rm -rf src/package
+
 # Install dependencies
 pip install -r requirements.txt -t src/package/
+
+# Copy the precompiled psycopg2-3.11 directory to the package directory
+cp -r src/psycopg2-3.11/* src/package/
 
 # Add your Lambda function code to the package
 cp src/lambda_function.py src/package/
