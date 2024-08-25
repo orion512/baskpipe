788d11f401596a058cb0b5fe6e1701140eacf504
Dominik <dominik.zulovecsajovic@mynestboxco.uk>
2024-08-25 18:05:35 +0100

    added step function via terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,3 +29,55 @@ resource "aws_iam_role_policy_attachment" "secrets_manager_access" {
   role       = aws_iam_role.sql_execute_role.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
+
+resource "aws_iam_role" "stepf_nba_daily_games_baskref_role" {
+  name = "stepf-nba-daily-games-baskref-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "states.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "stepf_nba_daily_games_baskref_policy" {
+  name   = "stepf-nba-daily-games-baskref-policy"
+  role   = aws_iam_role.stepf_nba_daily_games_baskref_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect: "Allow",
+        Action: [
+          "logs:CreateLogDelivery",
+          "logs:GetLogDelivery",
+          "logs:UpdateLogDelivery",
+          "logs:DeleteLogDelivery",
+          "logs:ListLogDeliveries",
+          "logs:PutResourcePolicy",
+          "logs:DescribeResourcePolicies",
+          "logs:DescribeLogGroups"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect: "Allow",
+        Action: [
+          "lambda:InvokeFunction"
+        ],
+        Resource = [
+          "${aws_lambda_function.baskpipe-daily-scrape.arn}",
+          "${aws_lambda_function.sql-execute.arn}"
+        ]
+      }
+    ]
+  })
+}
---
+++ terraform/step_functions.tf
@@ -0,0 +1,84 @@
+/**
+
+nba_daily_games_baskref
+
+get_data_scrape -------------
+
+{
+  "date.$": "$.date_day",
+  "s3_path": "s3://baskpipe/data/daily_games/"
+}
+
+OUTPUT WITH RESULTPATH
+$.combinedResult
+
+need CLOUDWATCH LOGS
+need execution role
+
+load_staging
+
+{
+  "s3_sql_path": "s3://baskpipe/sqls/dml/universal_s3_csv_load.sql",
+  "custom_params": {
+    "SCHEMA_NAME": "staging",
+    "TABLE_NAME": "st_daily_games",
+    "BUCKET_NAME": "baskpipe",
+    "S3_PATH.$": "States.Format('data/daily_games/{}_games.csv', $.date_day)",
+    "REGION_NAME": "eu-west-2"
+  },
+  "secret_name": "baskpipe_db_secret"
+}
+
+*/
+
+
+resource "aws_sfn_state_machine" "nba_daily_games_baskref" {
+  name     = "nba-daily-games-baskref"
+  role_arn = aws_iam_role.stepf_nba_daily_games_baskref_role.arn
+
+  definition = <<EOF
+  {
+    "Comment": "This is a pipeline for fetching, storing and procesing daily nba games.",
+    "StartAt": "get_data_scrape",
+    "States": {
+      "get_data_scrape": {
+        "Type": "Task",
+        "Resource": "${aws_lambda_function.baskpipe-daily-scrape.arn}",
+        "ResultPath": "$.combinedResult",
+        "Parameters": {
+            "date.$": "$.date_day",
+            "s3_path": "s3://baskpipe/data/daily_games/"
+        },
+        "Next": "init_staging"
+      },
+      "init_staging": {
+        "Type": "Task",
+        "Resource": "${aws_lambda_function.sql-execute.arn}",
+        "ResultPath": "$.combinedResult",
+        "Parameters": {
+          "s3_sql_path": "s3://baskpipe/sqls/ddl/st_daily_games.sql",
+          "custom_params": {},
+          "secret_name": "baskpipe_db_secret"
+        },
+        "Next": "load_staging"
+      },
+      "load_staging": {
+        "Type": "Task",
+        "Resource": "${aws_lambda_function.sql-execute.arn}",
+        "Parameters": {
+          "s3_sql_path": "s3://baskpipe/sqls/dml/universal_s3_csv_load.sql",
+          "custom_params": {
+            "SCHEMA_NAME": "staging",
+            "TABLE_NAME": "st_daily_games",
+            "BUCKET_NAME": "baskpipe",
+            "S3_PATH.$": "States.Format('data/daily_games/{}_games.csv', $.date_day)",
+            "REGION_NAME": "eu-west-2"
+          },
+          "secret_name": "baskpipe_db_secret"
+        },
+        "End": true
+      }
+    }
+  }
+  EOF
+}
