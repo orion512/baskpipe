64942124b1406d4e6fab4b86620809281112527d
Dominik <dominik.zulovecsajovic@mynestboxco.uk>
2024-09-19 07:23:17 +0100

    added season extraction to games and games_load

--- db/dml/games_load.sql
+++ db/dml/games_load.sql
@@ -1,16 +1,22 @@
-INSERT INTO public.games (br_game_id, br_game_url, game_time, arena_name, attendance, playin_game, playoff_game, playoff_conference, playoff_round, playoff_game_number)
+INSERT INTO public.games (br_game_id, br_game_url, season, game_time, arena_name, attendance, playin_game, playoff_game, playoff_conference, playoff_round, playoff_game_number)
 select
 	upper(trim(game_id)) as br_game_id,
 	trim(game_url) as br_game_url,
-	
+	CASE 
+        when
+        	(EXTRACT(MONTH FROM game_time::timestamp) > 9 AND EXTRACT(YEAR FROM game_time::timestamp) != 2020)
+        	OR (EXTRACT(MONTH FROM game_time::timestamp) >= 12 AND EXTRACT(YEAR FROM game_time::timestamp) = 2020)
+        THEN EXTRACT(YEAR FROM game_time::timestamp) + 1
+        ELSE EXTRACT(YEAR FROM game_time::timestamp)
+    END AS season,
 	game_time::timestamp,
 	initcap(trim(arena_name)) as arena_name,
-	attendance::int,
+	NULLIF(attendance, '')::int,
 	playin_game::bool,
 	playoff_game::bool,
 	trim(lower(playoff_conference)) as playoff_conference,
 	trim(lower(playoff_round)) as playoff_round,
-	trim(playoff_game_number)::int as playoff_game_number
+	NULLIF(trim(playoff_game_number), '')::int as playoff_game_number
 from staging.st_daily_games sdg
 where not exists (
 	select 1
